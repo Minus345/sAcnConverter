4bdce5c6febd1fada63c41f4253ff72a21738a68
Max <Max>
2023-08-14 12:55:43 +0200

    update

--- main.py
+++ main.py
@@ -1,19 +1,19 @@
 import sacn
 
 if __name__ == '__main__':
-    receiver = sacn.sACNreceiver()
+    receiver = sacn.sACNreceiver(bind_address='127.0.0.1')
     receiver.start()
     receiver.join_multicast(1)
 
     sender = sacn.sACNsender(source_name='sAcn Backup',
                              fps=40,
-                             bind_address='192.168.178.131')
+                             bind_address='192.168.178.187')
     sender.start()
-    sender.activate_output(2)
-    sender[2].multicast = True
-    sender[2].priority = 200
+    sender.activate_output(1)
+    sender[1].multicast = True
+    sender[1].priority = 100
 
 
     @receiver.listen_on('universe', universe=1)
     def callback(packet):
-        sender[2].dmx_data = packet.dmxData
+        sender[1].dmx_data = packet.dmxData
